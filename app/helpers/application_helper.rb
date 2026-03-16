b83e9fc7405c0c42b5f390ec1f09ca11048e49df
Kelvin Wachira <kwachira@outlook.com>
2026-03-16 12:56:39 +0300

    create community model to house submissions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
-    def render_svg(name, options={})
+    def render_svg(name, options = {})
         options[:title] ||= name.underscore.humanize
         options[:aria] = true
         options[:nocomment] = true
         options[:class] = options.fetch(:styles, "fill-current text-gray-500")
         filename = "#{name}.svg"
-        inline_svg_tag(filename, options) 
+        inline_svg_tag(filename, options)
+    end
+
+    def author_of?(resource)
+      user_signed_in? && current_user.id == resource.user_id
     end
 end
